56a7aff7f5a7cf33f2a28265ecd251b98c8bbfde
manavmeghpara <manav09meghpara@gmail.com>
2023-11-18 16:03:14 -0800

    basic stock query

--- app/src/main/java/com/example/financify/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/financify/ui/notifications/NotificationsViewModel.kt
@@ -3,11 +3,12 @@ package com.example.financify.ui.notifications
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.finnhub.api.models.Quote
 
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Navigation Fragment"
     }
     val text: LiveData<String> = _text
 }
