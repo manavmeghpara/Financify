03fb7a39ac652f2b4be6e823d85b33d368c6b16c
elishapdf <elisha.hayer@gmail.com>
2023-12-04 21:26:54 -0800

    new fragment edit_budget_category

--- app/src/main/java/com/example/financify/ui/budget/BudgetActivity.kt
+++ app/src/main/java/com/example/financify/ui/budget/BudgetActivity.kt
@@ -1,21 +1,13 @@
 package com.example.financify.ui.budget
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ListView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.animation.core.infiniteRepeatable
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.financify.R
-import com.example.financify.databinding.FragmentBudgetBinding
 import com.example.financify.ui.budget.DBs.BudgetDatabase
 import com.example.financify.ui.budget.DBs.CategoryAdapter
 import com.example.financify.ui.budget.DBs.CategoryDao
@@ -24,6 +16,7 @@ import com.example.financify.ui.budget.DBs.CategoryViewModel
 import com.example.financify.ui.budget.DBs.CategoryViewModelFactory
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.transition.MaterialElevationScale
+
 
 class BudgetActivity: AppCompatActivity() {
 
@@ -70,14 +63,17 @@ class BudgetActivity: AppCompatActivity() {
         budgetListView.adapter = categoryAdapter
 
         addCategoryButton.setOnClickListener() {
-            this.apply {
-                var exitTransition = MaterialElevationScale(false).apply {
-                    duration = resources.getInteger(R.integer.reply_motion_duration_large).toLong()
-                }
-                var reenterTransition = MaterialElevationScale(true).apply {
-                    duration = resources.getInteger(R.integer.reply_motion_duration_large).toLong()
-                }
-            }
+            supportFragmentManager.beginTransaction()
+                .add(com.example.financify.R.id.fragEditBudget, EditBudgetFragment()).commit()
+
+//            this.apply {
+//                var exitTransition = MaterialElevationScale(false).apply {
+//                    duration = resources.getInteger(com.example.financify.R.integer.reply_motion_duration_large).toLong()
+//                }
+//                var reenterTransition = MaterialElevationScale(true).apply {
+//                    duration = resources.getInteger(com.example.financify.R.integer.reply_motion_duration_large).toLong()
+//                }
+//            }
         }
 
 //        budgetListView.setOnItemClickListener { _, _, position, _ ->
