ee00a53a6b5d8c4765d1f68cb77a509681dfbbf3
Adam Dee <adammatthewdee@gmail.com>
2023-12-05 11:59:19 -0800

    Color code categories of expenses and purchases

--- app/src/main/java/com/example/financify/ui/budget/DBs/PurchasesAdapter.kt
+++ app/src/main/java/com/example/financify/ui/budget/DBs/PurchasesAdapter.kt
@@ -7,13 +7,20 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.example.financify.R
+import com.github.mikephil.charting.utils.ColorTemplate
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class PurchasesAdapter(
     context: Context,
-    private val categories: MutableList<Purchase>
-) : ArrayAdapter<Purchase>(context, android.R.layout.simple_list_item_2, categories) {
+    private val purchases: MutableList<Purchase>
+) : ArrayAdapter<Purchase>(context, android.R.layout.simple_list_item_2, purchases) {
+
+    private val colors = ColorTemplate.MATERIAL_COLORS.toMutableList()
+    private lateinit var categoryColorMap: Map<String, Int>
+    init {
+        initializeColorMap()
+    }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.listview_purchase, parent, false)
@@ -23,26 +30,42 @@ class PurchasesAdapter(
         val amountText: TextView = view.findViewById(R.id.amountTextView)
         val dateText: TextView = view.findViewById(R.id.dateTextView)
 
+        val purchase = getItem(position)
 
-        val date = getItem(position)?.dateTime
+        val date = purchase?.dateTime
         val dateFormat = SimpleDateFormat("MMM. d", Locale.getDefault())
         var formattedDate = ""
         if (date != null) {
             formattedDate = dateFormat.format(date.time)
         }
 
-        text1.text = "${getItem(position)?.name}"
-        amountText.text = "$${getItem(position)?.amount}"
-        text2.text = "${getItem(position)?.categoryName}"
+        text1.text = "${purchase?.name}"
+        amountText.text = "$${purchase?.amount}"
+        text2.text = "${purchase?.categoryName}"
         dateText.text = "${formattedDate}"
+
+        // Set color based on category
+        if (purchase != null) {
+            categoryColorMap[purchase.categoryName]?.let { text2.setTextColor(it) }
+        }
+
 
         return view
     }
 
     // Custom function to update the data in the adapter
     fun updateData(newPurchases: List<Purchase>) {
-        categories.clear()
-        categories.addAll(newPurchases)
+        purchases.clear()
+        purchases.addAll(newPurchases)
+        initializeColorMap()
         notifyDataSetChanged()
+    }
+
+    fun initializeColorMap() {
+        // Sort categories by name
+        val sortedCategories = purchases.map {it.categoryName}.distinct().sorted()
+
+        // Create a map from categoryName -> Color
+        categoryColorMap = sortedCategories.zip(colors).toMap()
     }
 }
